7c5159fba33d4564c1e0572dd8d709ecf141b29a
Pawel <pawelpiatek98@gmail.com>
2022-05-25 20:00:38 +0200

    added road animation

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from modules.warehouse_generator import generate_warehouse_data
 from modules.json_actions import read_json
-from modules.vizualization import display_points_plot
+from modules.vizualization import display_points_plot, display_road
 
-print(generate_warehouse_data())
+# print(generate_warehouse_data())
 
 json_array = read_json()
-display_points_plot(json_array)
+# display_points_plot(json_array)
+test_list = json_array[0:10]
+test_list.append(test_list[0])
+display_road(test_list)
--- modules/vizualization.py
+++ modules/vizualization.py
@@ -1,8 +1,36 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.animation import FuncAnimation
 
 def display_points_plot(data):
     df = pd.DataFrame(data)
     x, y = df['x'], df['y']
     plt.plot(x,y, 'bo')
     plt.show()
+
+
+
+def display_road(data):
+    df = pd.DataFrame(data)
+    x, y = df['x'], df['y']
+    fig, ax = plt.subplots()
+    line, = ax.plot(data[0]['x'], data[0]['y'])
+
+    x_data = []
+    y_data = []
+
+    x_data.append(data[0]['x'])
+    y_data.append(data[0]['y'])
+
+
+    def animation_frame(i):
+        if i < len(data):
+            x_data.append(data[i]['x'])
+            y_data.append(data[i]['y'])
+            plt.plot(x_data,y_data, color="blue")
+
+
+    ani = FuncAnimation(fig, animation_frame, frames=len(data), interval=100, repeat=False)
+    plt.show()
+
