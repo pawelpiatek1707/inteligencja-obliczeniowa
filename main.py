cdcbf2129ceb610b110b753d15725fa3d4100866
Pawel <pawelpiatek98@gmail.com>
2022-06-01 18:01:11 +0200

    finished multiple roads vizualization

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 from modules.warehouse_generator import generate_warehouse_data
 from modules.json_actions import read_json
-from modules.vizualization import display_points_plot, display_road
-
+from modules.vizualization import display_points_plot, display_road, display_multiple_roads
 # print(generate_warehouse_data())
 
 json_array = read_json()
+
+n = 20
+
+# using list comprehension
+splited_list = [json_array[i * n:(i + 1) * n] for i in range((len(json_array) + n - 1) // n )]
+
 # display_points_plot(json_array)
-test_list = json_array[0:10]
-test_list.append(test_list[0])
-display_road(test_list)
+# test_list = json_array[0:9]
+# test_list.append(test_list[0])
+
+# test_list2 = json_array[10:20]
+# test_list2.append(test_list2[0])
+# display_road(test_list)
+# display_road(test_list2)
+
+display_multiple_roads(splited_list)
--- modules/vizualization.py
+++ modules/vizualization.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import random
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.animation import FuncAnimation
@@ -13,9 +14,7 @@ def display_points_plot(data):
 
 def display_road(data):
     df = pd.DataFrame(data)
-    x, y = df['x'], df['y']
     fig, ax = plt.subplots()
-    line, = ax.plot(data[0]['x'], data[0]['y'])
 
     x_data = []
     y_data = []
@@ -33,4 +32,38 @@ def display_road(data):
 
     ani = FuncAnimation(fig, animation_frame, frames=len(data), interval=100, repeat=False)
     plt.show()
+
+
+def return_random_color():
+    return "%06x" % random.randint(0, 0xFFFFFF)
+
+
+
+def display_multiple_roads(data):
+
+    colors_list = ['FE0002', 'F9941E', 'FFFF01', '00FF03', '000000', '642D92']
+
+    plt.figure(figsize=(20, 15))
+    plt.title("Wizualizacja trasy przejazdu")
+    full_list = sum(data, [])
+    full_list_df = pd.DataFrame(full_list)
+    x, y = full_list_df['x'], full_list_df['y']
+    plt.plot(x,y, 'bo')
+
+    for index, row in enumerate(data):
+        df = pd.DataFrame(row)
+        x_array = df['x']
+        y_array = df['y']
+
+        random_color = None
+        if index > len(colors_list) - 1:
+            random_color = return_random_color()
+        else:
+            random_color = colors_list[index]
+
+        plt.plot(x_array,y_array, color=f"#{random_color}")
+
+
+    plt.show()
+
 
