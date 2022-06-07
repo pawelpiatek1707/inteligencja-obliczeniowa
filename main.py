3b144d934be2ec3917e37c039252498158b1fe53
Artur Leśnik <artur.lesnik@edu.uekat.pl>
2022-06-07 20:20:55 +0200

    dodano: generowanie 100 punktów odbioru towaru

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from modules.warehouse_generator import generate_warehouse_data
 from modules.json_actions import read_json
 from modules.vizualization import display_points_plot
 
-print(generate_warehouse_data())
+warehouse_data: list[dict[str, int]] = generate_warehouse_data()
 
 json_array = read_json()
 display_points_plot(json_array)
