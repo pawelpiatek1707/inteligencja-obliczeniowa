db57581f65e96fc8e2c183291d3f39ab6beeb54b
Artur Leśnik <artur.lesnik@edu.uekat.pl>
2022-06-07 20:48:34 +0200

    dodano: czasowo wyłączono pokazywanie mapy

--- main.py
+++ main.py
@@ -5,8 +5,3 @@ from modules.cars_collection import CarsCollection
 
 warehouse_data: list[dict[str, int]] = generate_warehouse_data()
 company_cars = CarsCollection()
-
-print(company_cars)
-
-json_array = read_json()
-display_points_plot(json_array)
