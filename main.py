74bdc6c132ec6f5b43e1a7b0248470afb35b3c2e
Artur Leśnik <artur.lesnik@edu.uekat.pl>
2022-06-08 19:45:27 +0200

    dodano: wizualizację magazynów i klientów na mapie

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from modules.warehouse_generator import generate_warehouse_data
 from modules.warehouse_generator import generate_storage_warehouse_data
-from modules.json_actions import read_json
 from modules.vizualization import display_points_plot
 from modules.cars_collection import CarsCollection
 from modules.path_manager import PathManager
@@ -8,6 +7,9 @@ from modules.path_manager import PathManager
 warehouse_data: list[dict[str, int]] = generate_warehouse_data()
 warehouse_storage_data: list[dict[str, int]] = generate_storage_warehouse_data()
 warehouse_all_locations: list[dict[str, int]] = warehouse_storage_data + warehouse_data
+
+display_points_plot(warehouse_data, warehouse_storage_data)
+
 company_cars: CarsCollection = CarsCollection()
 company_cars.place_in_start_warehouses()
 path_manager: PathManager = PathManager(warehouse_all_locations, company_cars)
--- modules/vizualization.py
+++ modules/vizualization.py
@@ -2,8 +2,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-def display_points_plot(data):
+def display_points_plot(data, data_2):
     df = pd.DataFrame(data)
+    df2 = pd.DataFrame(data_2)
     x, y = df['x'], df['y']
-    plt.plot(x,y, 'bo')
+    x2, y2 = df2['x'], df2['y']
+    plt.plot(x, y, 'bo')
+    plt.plot(x2, y2, 'ro')
     plt.show()
